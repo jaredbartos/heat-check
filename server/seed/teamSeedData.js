00186f470c8477481634c2da52a07ccfe4f9a74d
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-09 22:08:09 -0600

    Edit teamSeedData

--- server/seed/teamSeedData.js
+++ server/seed/teamSeedData.js
@@ -14,8 +14,6 @@ const leagues = [
 
 let teamSeedData = [];
 
-// Randomize matching team names into a league
-// Add the object to the seed data
 for (let i = 0; i < teamNames.length; i++) {
     const teamObj = { name: teamNames[i], league: leagues[(i % 2 === 0 ? 0 : 1)] };
     teamSeedData.push(teamObj);
