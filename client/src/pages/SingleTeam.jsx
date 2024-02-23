5fc9149d29d446bb1934c7120c08619d1bf4702f
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-22 21:34:23 -0600

    Troubleshoot team page layout shift

--- client/src/pages/SingleTeam.jsx
+++ client/src/pages/SingleTeam.jsx
@@ -218,7 +218,7 @@ export default function SingleTeam() {
                   </Center>
                 }
                 {
-                  loadingAverages
+                  loadingAverages && !averages
                   &&
                   <LoadingSpinner />
                 }
